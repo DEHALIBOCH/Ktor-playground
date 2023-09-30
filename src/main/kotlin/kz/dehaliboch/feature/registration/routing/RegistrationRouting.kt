c9bcde8fd85f2af05146c6debc76acd0373cce32
marat <almabekov.marat@gmail.com>
2023-10-01 01:47:08 +0600

    Registration routing

--- src/main/kotlin/kz/dehaliboch/feature/registration/routing/RegistrationRouting.kt
+++ src/main/kotlin/kz/dehaliboch/feature/registration/routing/RegistrationRouting.kt
@@ -4,8 +4,13 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kz.dehaliboch.cache.InMemoryCache
+import kz.dehaliboch.cache.InMemoryCache.usersList
+import kz.dehaliboch.cache.TokenCache
 import kz.dehaliboch.feature.registration.models.RegistrationRequest
+import kz.dehaliboch.feature.registration.models.RegistrationResponse
 import kz.dehaliboch.feature.registration.validators.validate
+import java.util.*
 
 fun Application.configureRegistrationRouting() {
 
@@ -19,7 +24,15 @@ fun Application.configureRegistrationRouting() {
                 return@post
             }
 
-            call.respond("Successful registration")
+            if (usersList.map { it.login }.contains(registrationRequest.login)) {
+                call.respondText("Email already taken")
+                return@post
+            }
+
+            val token = UUID.randomUUID().toString()
+            InMemoryCache.tokenList.add(TokenCache(registrationRequest.login, token))
+
+            call.respond(RegistrationResponse(token))
         }
     }
 }
