0e5ce981a999a53a30064f365fdbef1a3dc85c5d
marat <almabekov.marat@gmail.com>
2023-09-30 23:37:35 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "kz.dehaliboch.ktor-playground"
---
+++ src/main/kotlin/kz/dehaliboch/Application.kt
@@ -0,0 +1,15 @@
+package kz.dehaliboch
+
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import kz.dehaliboch.plugins.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
