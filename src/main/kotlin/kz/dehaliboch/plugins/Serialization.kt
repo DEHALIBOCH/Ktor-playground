86792527f8fcfd89f92b08b78e47a73516b49c80
marat <almabekov.marat@gmail.com>
2023-10-01 00:18:33 +0600

    Configure Serialization

---
+++ src/main/kotlin/kz/dehaliboch/plugins/Serialization.kt
@@ -0,0 +1,11 @@
+package kz.dehaliboch.plugins
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
