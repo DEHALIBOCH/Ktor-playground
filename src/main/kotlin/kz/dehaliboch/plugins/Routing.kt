2ebe6aee575e0c63b8633bff03cb42b53f806c9b
marat <almabekov.marat@gmail.com>
2023-10-01 00:17:16 +0600

    Sample GET methods

--- src/main/kotlin/kz/dehaliboch/plugins/Routing.kt
+++ src/main/kotlin/kz/dehaliboch/plugins/Routing.kt
@@ -3,11 +3,16 @@ package kz.dehaliboch.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kz.dehaliboch.common.generateSampleArray
+import kz.dehaliboch.models.test.TestModel
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respond(TestModel())
+        }
+        get("/sample_array") {
+            call.respond(generateSampleArray())
         }
     }
 }
