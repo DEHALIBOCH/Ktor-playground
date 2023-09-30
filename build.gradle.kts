23a1fd86b916c67c6b8dfe2e0d91149499259f4d
marat <almabekov.marat@gmail.com>
2023-10-01 00:16:30 +0600

    Serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.9.10"
     id("io.ktor.plugin") version "2.3.4"
+    kotlin("plugin.serialization") version "1.9.10"
 }
 
 group = "kz.dehaliboch"
@@ -26,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-cio-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
