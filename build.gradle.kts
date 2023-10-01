a815333d4605e22b854992c7467250c1b15718cf
marat <almabekov.marat@gmail.com>
2023-10-01 13:24:12 +0600

    Dependencies for database

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposedVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.9.10"
@@ -31,4 +32,9 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // Kotlin SQL framework
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 }
